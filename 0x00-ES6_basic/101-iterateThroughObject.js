2f13999efd09fa2607fbbd99c9b38f7bb60635fd
7amzawey <mohamedhamza123467890@gmail.com>
2024-10-02 11:32:20 +0200

    nice

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,15 @@
+export default function iterateThroughObject(reportWithIterator) {
+  let str = ``;
+  const iterator = reportWithIterator[Symbol.iterator]();
+  let result = iterator.next();
+  
+  while (!result.done) {
+    str += `${result.value}`;
+    result = iterator.next();
+    if (!result.done) {
+      str += ` | `;
+    }
+  }
+  return str;
+}
+
