c05cf4b1508c0b4d339109f9d668ab516995c6da
7amzawey <mohamedhamza123467890@gmail.com>
2024-10-04 09:39:12 +0200

    fine

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,10 +1,4 @@
 export default function appendToEachArrayValue(array, appendString) {
-  let i = 0;
-  for (let value of array) {
-    let newValue = appendString + value;
-    array[i] = newValue;
-    i++;
-  }
-
-  return array;
+  const newArray = array.map((value) => appendString + value);
+  return newArray;
 }
