4f67f98f17808dc68464e8cd2b7f959429119ee9
7amzawey <mohamedhamza123467890@gmail.com>
2024-10-04 11:26:46 +0200

    fixing lint

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,12 +1,11 @@
 export default function createIteratorObject(report) {
   const allEmployees = [];
   for (const department in report.allEmployees) {
-    if (report.allEmployees.hasOwnProperty(department)) {
+    if (department) {
       for (const employee of report.allEmployees[department]) {
         allEmployees.push(employee);
       }
     }
   }
-  return allEmployeesSymbol.iterator;
+  return allEmployees[Symbol.iterator]();
 }
-
