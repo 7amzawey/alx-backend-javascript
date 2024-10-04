a784680f6dfcbee46ad7d6fd3fdc8d425de521ea
7amzawey <mohamedhamza123467890@gmail.com>
2024-10-04 12:42:50 +0200

    5-building.js

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,7 +1,8 @@
 import Currency from './3-currency';
 
-export default class Pricing {
+export default class Pricing extends Currency {
   constructor(amount, currency) {
+    super(currency._code, currency._name);
     this._amount = amount;
     this._currency = currency;
   }
@@ -14,11 +15,19 @@ export default class Pricing {
     this._amount = value;
   }
 
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    this._currency = value;
+  }
+
   displayFullPrice() {
-    return (`${this._amount} ${this._currency._name} (${this._currency._code}`));
+    return `${this._amount} ${this.currency._name} (${this.currency._code})`;
   }
 
   convertPrice(amount, conversionRate) {
-    return (this._amount * conversionRate);
+    return this._amount * conversionRate;
   }
 }
