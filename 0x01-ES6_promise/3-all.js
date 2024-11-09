644060a351dfaad6fe131912305741de5e66c568
7amzawey <mohamedhamza123467890@gmail.com>
2024-11-09 15:47:43 +0200

    3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,10 +2,10 @@ import { createUser, uploadPhoto } from './utils';
 
 export default async function handleProfileSignup() {
   try {
-    const photo = await (await uploadPhoto()).body;
-    const user = await (await createUser());
+    const photo = (await uploadPhoto()).body;
+    const user = (await createUser());
     console.log(`${photo} ${user.firstName} ${user.lastName}`);
   } catch (error) {
-    console.error('Signup system offline');
+    console.error('Signup system offline', error);
   }
 }
--- 0x01-ES6_promise/test.js
+++ 0x01-ES6_promise/test.js
@@ -1,20 +1,14 @@
-let promise = new Promise((resolve, reject) => 
-	{
-		setTimeout(() => {
-			resolve("Step 1 Complete")
-		}, 1000);
-	});
-promise
-	.then((message) => {
-		console.log(message);
-		return new Promise((resolve, reject) => 
-			{
-			setTimeout(() => {
-				resolve("Step 2 complete")
-			}, 1000);
-			});
-	})
-	.then((message) => {
-		console.log(message);
-	});
+import handleProfileSignup from "./3-all";
+import * as utilsFunctions from "./utils";
 
+test("handleProfileSignup manage an error", async () => {
+  const spy = jest.spyOn(console, "log").mockImplementation();
+  const uploadPhotoSpy = jest.spyOn(utilsFunctions, 'uploadPhoto').mockImplementation();
+  uploadPhotoSpy.mockImplementation(() => {
+    return Promise.reject(() => {});
+  });
+
+  await handleProfileSignup();
+  expect(spy).toBeCalledWith('Signup system offline');
+  spy.mockRestore();
+});
