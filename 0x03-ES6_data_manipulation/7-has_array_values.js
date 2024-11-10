84ede78d4b4ce77292d466af3a598f3ceed56579
7amzawey <mohamedhamza123467890@gmail.com>
2024-11-10 15:09:23 +0200

    7-has_array_values.js

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,4 +1,4 @@
 export default function hasValuesFromArray(set, array) {
-  const checkEveryElement = array.every(Element => set.has(Element));
+  const checkEveryElement = array.every((Element) => set.has(Element));
   return checkEveryElement;
 }
