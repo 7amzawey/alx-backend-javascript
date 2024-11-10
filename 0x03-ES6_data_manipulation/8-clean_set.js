c908ecc0dfa3140d2cff74b1d27d03af440ddaab
7amzawey <mohamedhamza123467890@gmail.com>
2024-11-10 15:46:27 +0200

    8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,8 @@
 export default function cleanSet(set, startString) {
+  if (startString === '') {
+    return '';
+  }
+
   const res = Array.from(set)
     .filter((Element) => Element.startsWith(startString))
     .map((prefix) => prefix.slice(startString.length)).join('-');
