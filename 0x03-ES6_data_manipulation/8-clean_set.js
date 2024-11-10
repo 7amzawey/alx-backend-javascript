1aef393fda5906d731bc8dc322479f1e69beb8a1
7amzawey <mohamedhamza123467890@gmail.com>
2024-11-10 16:07:23 +0200

    8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,10 @@
 export default function cleanSet(set, startString) {
-    if (typeof startString !== 'string' || !startString) {
-      return '';
-    }
-  
-    const res = Array.from(set)
-      .filter((element) => element.startsWith(startString))
-      .map((prefix) => prefix.slice(startString.length))
-      .join('-');
-    return res;
+  if (typeof startString !== 'string' || !startString) {
+    return '';
   }
-  
+  const res = Array.from(set)
+    .filter((element) => element.startsWith(startString))
+    .map((prefix) => prefix.slice(startString.length))
+    .join('-');
+  return res;
+}
