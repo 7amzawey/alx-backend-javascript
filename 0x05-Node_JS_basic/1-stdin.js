11a4718ecd0eb27f0c826af2327d1a3196b66cc5
7amzawey <mohamedhamza123467890@gmail.com>
2024-11-27 08:38:57 +0200

    1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,16 +1,13 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+/* global process */
 
-if (process.stdin.isTTY) {
-  process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data.toString()}`);
-    process.exit();
-  });
-} else {
-  process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data.toString()}`);
-    process.exit();
-  });
-  process.on('exit', () => {
-    process.stdout.write('This important software is now closing\n');
-  });
-}
+process.stdout.write('Welcome to Holberton School, what is your name?\n')
+process.stdin.on('data', function(data) {
+  process.stdout.write(`Your name is: ${data}`)
+  process.exit()
+});
+
+process.on('exit', function() {
+  if (!process.stdin.isTTY) {
+    process.stdout.write('This important software is now closing\n')
+  }
+});
