339ee322c8d3e35243b90ef98fd728116e71652f
7amzawey <mohamedhamza123467890@gmail.com>
2024-11-27 08:50:44 +0200

    fine

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,5 +1,3 @@
-/* global process */
-
 process.stdout.write('Welcome to Holberton School, what is your name?\n')
 process.stdin.on('data', function(data) {
   process.stdout.write(`Your name is: ${data}`)
