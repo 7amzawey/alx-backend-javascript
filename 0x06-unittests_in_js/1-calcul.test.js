cd85b43d4060c71796af46c62e49cd2437282f81
7amzawey <mohamedhamza123467890@gmail.com>
2024-11-28 16:05:54 +0200

    checking

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -3,16 +3,16 @@ const calculateNumber = require('./1-calcul')
 
 describe('calculateNumber', function() {
   it('return 4', function() {
-      assert.strictEqual(calculateNumber('SUM', 1, 3), 4);
+    assert.strictEqual(calculateNumber('SUM', 1, 3), 4);
   });
   it('return 5', function() {
-      assert.strictEqual(calculateNumber('SUM', 1, 3.7), 5);
+    assert.strictEqual(calculateNumber('SUM', 1, 3.7), 5);
   });
   it('return 3', function() {
-      assert.strictEqual(calculateNumber('SUBTRACT', 1.2, 3.7), -3);
+    assert.strictEqual(calculateNumber('SUBTRACT', 1.2, 3.7), -3);
   });
   it('return 0.5', function() {
-      assert.strictEqual(calculateNumber('DIVIDE', 1.5, 3.7), 0.5);
+    assert.strictEqual(calculateNumber('DIVIDE', 1.5, 3.7), 0.5);
   });
   it('return Error', function() {
     assert.strictEqual(calculateNumber('DIVIDE', 1.5, 0.2), 'Error')
--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -4,18 +4,18 @@ const calculateNumber = require('./1-calcul')
 
 describe('calculateNumber', function() {
   it('return 4', function() {
-      expect(calculateNumber('SUM', 1, 3)).to.equal(4);
+    expect(calculateNumber('SUM', 1, 3)).to.equal(4);
   });
   it('return 5', function() {
-      expect(calculateNumber('SUM', 1, 3.7)).to.equal(5);
+    expect(calculateNumber('SUM', 1, 3.7)).to.equal(5);
   });
   it('return 3', function() {
-      expect(calculateNumber('SUBTRACT', 1.2, 3.7)).to.equal(-3);
+    expect(calculateNumber('SUBTRACT', 1.2, 3.7)).to.equal(-3);
   });
   it('return 0.5', function() {
-      chai.expect(calculateNumber('DIVIDE', 1.5, 3.7)).to.equal(0.5);
+    expect(calculateNumber('DIVIDE', 1.5, 3.7)).to.equal(0.5);
   });
   it('return Error', function() {
-    chai.expect(calculateNumber('DIVIDE', 1.5, 0.2)).to.equal('Error');
+    expect(calculateNumber('DIVIDE', 1.5, 0.2)).to.equal('Error');
   })
 });
