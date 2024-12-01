f601ba8b73cb51e2c27c458f0cf216c0f134309b
7amzawey <mohamedhamza123467890@gmail.com>
2024-12-01 08:10:56 +0200

    fixing stuff

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,16 +1,22 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const { expect } = require('chai');
-const app = require('./api');
+const { expect } = chai;
+const { server, app } = require('./api');
 
 chai.use(chaiHttp);
 
 describe('A test suite for the api.', function() {
+  after(function(done) {
+    server.close(done);
+  });
+
   it('Returns Welcome to the payment system.', function(done) {
-    chai.request(app).get('/').end((err, res) => {
-      expect(res).to.have.status(200);
-      expect(res.text).to.equal('Welcome to the payment system');
-      done();
-    });
+    chai.request(app)
+      .get('/')
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.text).to.equal('Welcome to the payment system');
+        done();
+      });
   });
 });
