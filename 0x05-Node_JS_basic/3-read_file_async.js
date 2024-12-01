25e682fca7cf3387ae39bc861e6c535b83698aec
7amzawey <mohamedhamza123467890@gmail.com>
2024-12-01 13:25:19 +0200

    finally fixed it

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -5,28 +5,23 @@ function countStudents(path) {
     fs.readFile(path, 'utf-8', (err, data) => {
       if (err) {
         reject(new Error('Cannot load the database'));
-        return;
-      }
-
-      const lines = data.split('\n').filter((line) => line.trim() !== '');
-      const students = lines.slice(1);
-
-      console.log(`Number of students: ${students.length}`);
-
-      const fields = {};
-      students.forEach((student) => {
-        const [firstName, , , field] = student.split(',');
-        if (!fields[field]) {
-          fields[field] = [];
+      } else {
+        const lines = data.split('\n').filter((line) => line.trim() !== '');
+        const students = lines.slice(1);
+        const fields = {};
+        students.forEach((student) => {
+          const [firstName, , , field] = student.split(',');
+          if (!fields[field]) {
+            fields[field] = [];
+          }
+          fields[field].push(firstName);
+        });
+        let result = `Number of students: ${students.length}\n`;
+        for (const [field, fNames] of Object.entries(fields)) {
+          result += `Number of students in ${field}: ${fNames.length}. List: ${fNames.join(', ')}\n`;
         }
-        fields[field].push(firstName);
-      });
-      let result = `Number of students: ${students.length}\n`;
-      for (const [field, fNames] of Object.entries(fields)) {
-        result += (`Number of students in ${field}: ${fNames.length}. List: ${fNames.join(', ')}\n`);
+        resolve(result.trim());
       }
-
-      resolve(result.trim());
     });
   });
 }
