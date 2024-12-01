51be33ffa902aeb7eaf2036624b78a712b652b09
7amzawey <mohamedhamza123467890@gmail.com>
2024-12-01 10:02:31 +0200

    linting

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,14 @@
+const process = require('process');
+
 process.stdout.write('Welcome to Holberton School, what is your name?\n')
 
 process.stdin.on('data', function(data) {
-  process.stdout.write(`Your name is: ${data}`)
-  process.exit()
+  process.stdout.write(`Your name is: ${data}`);
+  process.exit();
 });
 
 process.on('exit', function() {
   if (!process.stdin.isTTY) {
-    process.stdout.write('This important software is now closing\n')
+    process.stdout.write('This important software is now closing\n');
   }
 });
