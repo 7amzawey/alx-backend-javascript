4ccc90db3c8446668098e4f4d971b9ecf84cac21
7amzawey <mohamedhamza123467890@gmail.com>
2024-12-01 13:22:07 +0200

    fixed it

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,18 +1,25 @@
 const express = require('express');
-
 const countStudents = require('./3-read_file_async');
 
-const app = express('The server is running mate');
+const app = express();
 const PORT = 1245;
+
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
+
 app.get('/students', async (req, res) => {
   const database = process.argv[2];
-  const data = await countStudents(database);
-  res.send(`This is the list of our students\n${data}`);
+  try {
+    const data = await countStudents(database);
+    res.send(`This is the list of our students\n${data}`);
+  } catch (error) {
+    res.status(500).send('Cannot load the database');
+  }
 });
+
 app.listen(PORT, () => {
   console.log('Server is starting mate');
 });
+
 module.exports = app;
