2ace909d424f0d8043631c2ffcf1e4a8be27097e
7amzawey <mohamedhamza123467890@gmail.com>
2024-12-01 10:06:49 +0200

    let's see

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,13 @@
 const process = require('process');
 
-process.stdout.write('Welcome to Holberton School, what is your name?\n')
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', function(data) {
+process.stdin.on('data', (data) => {
   process.stdout.write(`Your name is: ${data}`);
   process.exit();
 });
 
-process.on('exit', function() {
+process.on('exit', () => {
   if (!process.stdin.isTTY) {
     process.stdout.write('This important software is now closing\n');
   }
