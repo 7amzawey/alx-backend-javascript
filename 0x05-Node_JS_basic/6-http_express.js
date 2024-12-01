2a5e90a5cd13889d38a68342f79d84d6cfe22bb2
7amzawey <mohamedhamza123467890@gmail.com>
2024-12-01 13:19:29 +0200

    linting...

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,13 +1,14 @@
 const express = require('express');
+
 const port = 1245;
-const app = express("The sever is running on port 1245");
+const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
 app.listen(port, () => {
-    console.log('sever is running on port 1245');
+  console.log('sever is running on port 1245');
 });
 
 module.exports = app;
