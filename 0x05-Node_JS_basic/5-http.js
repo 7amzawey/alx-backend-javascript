e8bb0d2487925b4c20ee1c66da959a8b2d7fe50d
7amzawey <mohamedhamza123467890@gmail.com>
2024-12-01 13:09:12 +0200

    fineistien

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,12 +1,13 @@
 const express = require('express');
+
 const countStudents = require('./3-read_file_async');
 const app = express('The server is running mate');
 const PORT = 1245;
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
-app.get('/students', async(req, res) => {
-  database = process.argv[2];
+app.get('/students', async (req, res) => {
+  const database = process.argv[2];
   const data = await countStudents(database);
   res.send(`This is the list of our students\n${data}`);
 });
